1a7a41a073acc78c37ce297793e19f42ae670fea
KyleHaggin <KyleHaggin@users.noreply.github.com>
2020-02-13 08:05:20 -0800

    project begin

--- src/adv.py
+++ src/adv.py
@@ -9,12 +9,12 @@ room = {
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you,
+falling into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from
+west to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
@@ -33,9 +33,13 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
-# Main
-#
+
+# Initialize the new player
+'''code here'''
+
+# Actual game loop here
+while true:
+    break
 
 # Make a new player object that is currently in the 'outside' room.
 
